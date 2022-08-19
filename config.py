a5ba0947d4e4d528949190179740c09fc412d8e6
aerastov <a.erastov@gmail.com>
2022-08-19 23:43:27 +0300

    create repo for resume

---
+++ config.py
@@ -0,0 +1,7 @@
+TOKEN_BOT = r"1600497880:AAGCPupk1uFarTPPw45TY0fux8iZOZm69bA"
+API_KEY = "fbd38bdf2013b40abda7"
+list_of_currencies = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB'
+}
